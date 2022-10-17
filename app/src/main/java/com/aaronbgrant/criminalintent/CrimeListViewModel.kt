ee40d3a9575243b955c6d734535e6606e3d8ec35
Aaron Grant <aaron.b.grant@gmail.com>
2022-10-16 21:42:25 -0500

    Nit

--- app/src/main/java/com/aaronbgrant/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/aaronbgrant/criminalintent/CrimeListViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class CrimeListViewModel : ViewModel() {
+
     private val crimeRepository = CrimeRepository.get()
 
     private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
